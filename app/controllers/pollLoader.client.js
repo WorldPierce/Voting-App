2f681ddb662af0e95e8208008d9614c7a2b8d9cf
William Pierce <wffpierce@gmail.com>
2017-07-12 15:53:34 +0000

    list with flex display

--- app/controllers/pollLoader.client.js
+++ app/controllers/pollLoader.client.js
@@ -14,13 +14,13 @@
         if(poll.title != undefined){
             appendPolls = '<li>';
             console.log(poll.title);
-            appendPolls += '<p><strong>' + poll.title + '</strong></p>';
+            appendPolls += '<a href="#" class="toggle"><p><strong>' + poll.title + '</strong></p></a><div title="'+poll.title+'"class="drop">';
             var options = poll.options
             options.map(function(possibleOption){
                 console.log(possibleOption.name);
                 appendPolls += '<p>' + possibleOption.name + ': ' + possibleOption.votes + '</p>';
             })
-            appendPolls += '</li>';
+            appendPolls += '</div></li>';
     //pollNbr.appendChild(appendPolls);
             $( "#polls" ).append(appendPolls);
           }
@@ -32,3 +32,21 @@
 
 
 })();
+
+(function($) {
+  var tabs =  $(".tabs li a");
+  tabs.click(function() {
+    var content = this.hash.replace('/','');
+    tabs.removeClass("active");
+    $(this).addClass("active");
+    $("#content").find('div').hide();
+    $(content).fadeIn(200);
+  });
+})(jQuery);
+
+
+$('#polls').on('click', '.toggle', function(e) {
+    // do something
+    $(this).toggleClass('active').next().slideToggle("fast");
+    e.preventDefault();
+});
